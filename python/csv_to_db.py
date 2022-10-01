ce92a416d41ff702e120656e0f43d15fec5c3ef8
amandah6 <84157211+amandah6@users.noreply.github.com>
2022-10-01 11:35:52 -0500

    Added unit test through pytest

--- python/csv_to_db.py
+++ python/csv_to_db.py
@@ -5,26 +5,31 @@ import pandas as pd
 connection = sqlite3.connect("gpa_dataset.db")
 cursor = connection.cursor()
 
-# read csv
-df = pd.read_csv("uiuc-gpa-dataset.csv")
-# remove duplicates, keeping first occurance
-df_courses = df.drop_duplicates(subset=['Course Title'])
-df_courses = df_courses[['Year', 'Term', 'YearTerm', 'Subject', 'Number', 'Course Title']]
 
-# populate db
-df_courses.to_sql('courses', connection, if_exists='replace', index=False)
-print(pd.read_sql('SELECT * FROM courses LIMIT 10', connection))
+# remove duplicates, keeping first occurance
+def make_courses_table(connection):
+    # read csv
+    df = pd.read_csv("uiuc-gpa-dataset.csv")
+    df = df.drop_duplicates(subset=['Course Title'])
+    df = df[['Year', 'Term', 'YearTerm', 'Subject', 'Number', 'Course Title']]
+
+    # populate db
+    df.to_sql('courses', connection, if_exists='replace', index=False)
+
 
 
 # gpa table
-# create column with subject + number
-df['ID'] = df['Subject'] + df['Number'].astype('str')
-col_id = df.pop('ID')
-df.insert(0, 'ID', col_id)
-df = df.drop(['Year', 'Term', 'Sched Type', 'Course Title', 'Subject', 'Number'], axis=1)
-
-df.to_sql('gpa', connection, if_exists='replace', index=False)
-print(pd.read_sql('SELECT * FROM gpa LIMIT 30', connection))
-
+def make_gpa_table(connection):
+    # read csv
+    df = pd.read_csv("uiuc-gpa-dataset.csv")
+    # create column with subject + number
+    df['ID'] = df['Subject'] + df['Number'].astype('str')
+    col_id = df.pop('ID')
+    df.insert(0, 'ID', col_id)
+    df = df.drop(['Year', 'Term', 'Sched Type', 'Course Title', 'Subject', 'Number'], axis=1)
+    df.sort_values(by=['ID', 'YearTerm'], axis=0)
+    # populate db
+    df.to_sql('gpa', connection, if_exists='replace', index=False)
+    # print(pd.read_sql('SELECT * FROM gpa WHERE ID="SOC275" LIMIT 30', connection))
 
 connection.close()
