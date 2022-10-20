f12ffa2db9d9e41750f023fdd909ccda881b9537
Nathan Parekh <33500247+nathanparekh@users.noreply.github.com>
2022-10-19 19:10:38 -0500

    Scaffolding and a few placeholder commands for Cobra CLI application (#16)
    Initial scaffolding for Cobra application

---
+++ cli/cmd/courses.go
@@ -0,0 +1,36 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func coursesFunc(cmd *cobra.Command, args []string) {
+	fmt.Println("Entire list of courses")
+}
+
+// coursesCmd represents the courses command
+var coursesCmd = &cobra.Command{
+	Use:   "courses",
+	Short: "Lists all courses",
+	Long:  `Entire listing of courses for the University of Illinois at Urbana-Champaign`,
+	Run: coursesFunc,
+}
+
+func init() {
+	rootCmd.AddCommand(coursesCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// coursesCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// coursesCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
--- cli/main.go
+++ cli/main.go
@@ -1,8 +1,12 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+*/
 package main
 
-import "fmt"
+import "github.com/CS222-UIUC/course-project-group-10.git/cli/cmd"
 
 func main() {
 	fmt.Println("hello")
 	fmt.Println("hey")
+	cmd.Execute()
 }
