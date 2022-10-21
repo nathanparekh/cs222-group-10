12c38c0df98e59ade6e68116c25d8be3faf7efc1
Amanda <amandah14344@gmail.com>
2022-10-21 12:48:40 -0500

    fixed typo and removed unused folder

--- cli/tests/db_test.go
+++ cli/tests/db_test.go
@@ -20,24 +20,24 @@ func TestCanOpenDB(t *testing.T) {
 	db.Close()
 }
 
-// func TestCourseDataIntegrity(t *testing.T) {
-// 	db, _ := sql.Open("sqlite3", "python/gpa_dataset.db")
-// 	courses, err := db.Query("SELECT * FROM courses")
-// 	if err != nil {
-// 		t.Errorf("couldn't query courses")
-// 	}
+func TestCourseDataIntegrity(t *testing.T) {
+	db, _ := sql.Open("sqlite3", "../../python/gpa_dataset.db")
+	courses, err := db.Query("SELECT * FROM courses")
+	if err != nil {
+		t.Errorf("couldn't query courses")
+	}
 
-// 	cols, err := courses.Columns()
+	cols, err := courses.Columns()
 
-// 	if err != nil {
-// 		t.Errorf("error parsing columns")
-// 	}
+	if err != nil {
+		t.Errorf("error parsing columns")
+	}
 
-// 	if len(cols) != 6 {
-// 		t.Errorf("incorrect number of columns")
-// 	}
-// 	db.Close()
-// }
+	if len(cols) != 6 {
+		t.Errorf("incorrect number of columns")
+	}
+	db.Close()
+}
 
 func TestGetCourseByNum(t *testing.T) {
 	var course data.Course = data.GetCourseByNum("CS", 225)
