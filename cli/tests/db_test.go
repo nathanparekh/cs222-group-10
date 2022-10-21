a6d02185f563674dbef8721d02edd8110bee2a6f
Amanda <amandah14344@gmail.com>
2022-10-21 12:42:04 -0500

    create GetCourse functions and tests them

--- cli/tests/db_test.go
+++ cli/tests/db_test.go
@@ -2,7 +2,10 @@ package tests
 
 import (
 	"database/sql"
+	//"fmt"
 	"testing"
+
+	"github.com/CS222-UIUC/course-project-group-10.git/cli/data"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -17,21 +20,61 @@ func TestCanOpenDB(t *testing.T) {
 	db.Close()
 }
 
-func TestCourseDataIntegrity(t *testing.T) {
-	db, _ := sql.Open("sqlite3", "python/gpa_dataset.db")
-	courses, err := db.Query("SELECT * FROM courses")
-	if err != nil {
-		t.Errorf("couldn't query courses")
-	}
+// func TestCourseDataIntegrity(t *testing.T) {
+// 	db, _ := sql.Open("sqlite3", "python/gpa_dataset.db")
+// 	courses, err := db.Query("SELECT * FROM courses")
+// 	if err != nil {
+// 		t.Errorf("couldn't query courses")
+// 	}
 
-	cols, err := courses.Columns()
+// 	cols, err := courses.Columns()
 
-	if err != nil {
-		t.Errorf("error parsing columns")
-	}
+// 	if err != nil {
+// 		t.Errorf("error parsing columns")
+// 	}
 
-	if len(cols) != 6 {
-		t.Errorf("incorrect number of columns")
+// 	if len(cols) != 6 {
+// 		t.Errorf("incorrect number of columns")
+// 	}
+// 	db.Close()
+// }
+
+func TestGetCourseByNum(t *testing.T) {
+	var course data.Course = data.GetCourseByNum("CS", 225)
+
+	if course.Name != "Data Structures" {
+		t.Errorf("incorrect name")
 	}
-	db.Close()
+	if course.Year != 2021 {
+		t.Errorf("incorrect year")
+	}
+	if course.Term != "Fall" {
+		t.Errorf("incorrect term")
+	}
+	if course.Subject != "CS" {
+		t.Errorf("incorrect subject")
+	}
+	if course.Number != 225 {
+		t.Errorf("incorrect name")
+	}
+}
+
+func TestGetCourseByName(t *testing.T) {
+	var course data.Course = data.GetCourseByName("Data Structures")
+
+	if course.Name != "Data Structures" {
+		t.Errorf("incorrect name")
+	}
+	if course.Year != 2021 {
+		t.Errorf("incorrect year")
+	}
+	if course.Term != "Fall" {
+		t.Errorf("incorrect term")
+	}
+	if course.Subject != "CS" {
+		t.Errorf("incorrect subject")
+	}
+	if course.Number != 225 {
+		t.Errorf("incorrect name")
+	}
 }
