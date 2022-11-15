2940d92101522e766ba96c1ec3a4d67b22574f95
jamieRollison <rollisonjamie15@gmail.com>
2022-11-14 18:23:04 -0600

    deleted obsolete go commands and changed root description

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -12,16 +12,10 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "course-project-group-10.git",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "Get info about UIUC courses",
+	Long: `Command line tool for accessing information
+	regarding courses at UIUC. Can list course details including
+	meeting times, GPA data, fulfilled gen ed requirements, etc.`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
